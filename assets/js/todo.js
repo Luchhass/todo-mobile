5c3fdaed0317dcc0aa04fb686d9f4e8eeeb3c333
Furkan Coşar <furkancosar2005@gmail.com>
2025-07-25 06:27:07 +0300

    ui fixes

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,44 +1,44 @@
-let contactForm = document.querySelector('.form-group');
-let lists = document.querySelector('.lists');
-let resetButton = document.querySelector('.reset');
+let contactForm = document.querySelector(".form-group");
+let lists = document.querySelector(".lists");
+let resetButton = document.querySelector(".reset");
 
 let todoİtems = [];
 
-if(typeof localStorage.todoİtems !== 'undefined') {
-    todoİtems = JSON.parse(localStorage.todoİtems);
-    renderSubmitForm();
+if (typeof localStorage.todoİtems !== "undefined") {
+  todoİtems = JSON.parse(localStorage.todoİtems);
+  renderSubmitForm();
 }
 
 function handleSubmitForm(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    let formdata = new FormData(contactForm);
-    let formobj = Object.fromEntries(formdata);
-    todoİtems.push(formobj);
+  let formdata = new FormData(contactForm);
+  let formobj = Object.fromEntries(formdata);
+  todoİtems.push(formobj);
 
-    contactForm.reset();
-    renderSubmitForm();
-    save();
+  contactForm.reset();
+  renderSubmitForm();
+  save();
 }
 
-contactForm.addEventListener('submit', handleSubmitForm);
+contactForm.addEventListener("submit", handleSubmitForm);
 
 function save() {
-    localStorage.todoİtems =JSON.stringify(todoİtems);
+  localStorage.todoİtems = JSON.stringify(todoİtems);
 }
 
-function renderSubmitForm() { 
-    lists.innerHTML = ''
-    for(let i = 0; i < todoİtems.length; i++) {
-        lists.innerHTML += `<li>${todoİtems[i].todo}</li>`
-    }
+function renderSubmitForm() {
+  lists.innerHTML = "";
+  for (let i = 0; i < todoİtems.length; i++) {
+    lists.innerHTML += `<li>${todoİtems[i].todo}</li>`;
+  }
 }
 
 function reset() {
-    localStorage.clear();
+  localStorage.clear();
 
-    todoİtems = [];
-    renderSubmitForm();
+  todoİtems = [];
+  renderSubmitForm();
 }
 
-resetButton.addEventListener('click',reset);
+resetButton.addEventListener("click", reset);
